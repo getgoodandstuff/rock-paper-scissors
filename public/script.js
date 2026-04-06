fc9b6b16a0fef46426a0f89e36fd59272c6edc01
Brefkaa <brefkaa@gmatc.matc.edu>
2026-04-05 20:48:23 -0500

    deploy ready

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000");
+const socket = io();
 
 const submitBtn = document.getElementById("submit");
 const selectionsEl = document.getElementById("selections");
